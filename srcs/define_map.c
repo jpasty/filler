cd7fed045d2eff4e83d9717d2313039b7db2be27
jpasty <jpasty@student.21-school.ru>
2020-08-22 19:48:55 +0300

    try to get content from raws

--- srcs/define_map.c
+++ srcs/define_map.c
@@ -6,19 +6,37 @@
 /*   By: jpasty <jpasty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/22 15:30:54 by jpasty            #+#    #+#             */
-/*   Updated: 2020/08/22 17:59:38 by jpasty           ###   ########.fr       */
+/*   Updated: 2020/08/22 19:47:07 by jpasty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-static void	get_content_from_raw(char *raw, t_cell ***cells)
+static int		get_content_from_raw(char *raw, t_cell ***cells)
 {
-	
+	static int	height = 0;
+	int 		i;
+
+	i = 0;
+	while (raw)
+	{
+		//it's doesn't workingÅ
+		(**cells)->crd.x = i;
+		(**cells)->crd.y = height;
+		(**cells)->cntnt = raw[i];
+		if ((**cells)->cntnt == 'o' || (**cells)->cntnt == 'x')
+			(**cells)->cntnt = ft_toupper((**cells)->cntnt);
+		if ((**cells)->cntnt != '.' && (**cells)->cntnt != 'O'
+			&& (**cells)->cntnt != 'X')
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
 }
-static void	get_plateau_size(t_plateau *plt, char *line)
+
+static void		get_plateau_size(t_plateau *plt, char *line)
 {
-	char 	**split;
+	char 		**split;
 
 	split = ft_strsplit(line, ' ');
 	if (split[1] && split[2])
@@ -30,18 +48,18 @@ static void	get_plateau_size(t_plateau *plt, char *line)
 	ft_free_split(split);
 }
 
-void 		define_map(t_contest *cntst, char *line)
+void 			define_map(t_contest *cntst, char *line)
 {
-	char	*raw;
+	char		*raw;
 
 	if (gnl(STDIN_FILENO, &line) == 1)
 	 	get_plateau_size(&(cntst->plat), line);
-	gnl(STDIN_FILENO, line);
+	gnl(STDIN_FILENO, &line);
 	free(line);
 	while (gnl(STDIN_FILENO, &line) == 1 && !ft_strnequ(line, "Piece", 5))
 	{
 		raw = ft_strchr(line, ' ');
-		get_content_from_raw(raw, cntst->plat.cells);
+		get_content_from_raw(raw, &cntst->plat.cells);
 		free(line);
 	}
 }
